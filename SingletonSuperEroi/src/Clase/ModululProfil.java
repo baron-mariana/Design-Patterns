7710650b42dd9ac358fc2de7ed8892ef24094253
Mariana Baron <baronmariana28@gmail.com>
2021-03-26 12:21:34 +0200

    Singleton Baza de Date -Creationale

---
+++ SingletonSuperEroi/src/Clase/ModululProfil.java
@@ -0,0 +1,10 @@
+package Clase;
+
+public class ModululProfil {
+	
+	public ModululProfil() {
+		//ConexiuneBD conexiune = new ConexiuneBD("10.0.0.1", "superGame");
+		ConexiuneBD conexiune = ConexiuneBD.getConexiune();
+	}
+
+}
